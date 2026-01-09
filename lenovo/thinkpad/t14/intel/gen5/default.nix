fa52c724fcea750be32ae1d323e488302f48974f
robot-o <robot-o@users.noreply.github.com>
2026-01-09 18:43:35 +0100

    feat: add lenovo-thinkpad-t14-intel-gen5 config

---
+++ lenovo/thinkpad/t14/intel/gen5/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+
+{
+  imports = [
+    ../../.
+    ../../../../../common/cpu/intel/meteor-lake
+  ];
+
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.8") pkgs.linuxPackages_latest;
+
+  # TODO: Looking for feedback - modern Intel CPUs don't typically need throttled service
+  # which can interfere with newer power management on Meteor Lake
+  services.throttled.enable = lib.mkDefault false;
+}
